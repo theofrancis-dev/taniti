b6bf1d8cff765fc234f9f7df8a35258e6fa5506e
aiquilibrium <aiquilibrium@proton.me>
2025-03-06 06:29:46 -0500

    reviews

--- main.js
+++ main.js
@@ -24,8 +24,18 @@
       "url"    : "https://www.pexels.com/photo/nighttime-view-of-lawson-convenience-store-30037323/"
     },
     "photo" : {
-      
+      "author": "Joel Abroad",
+      "url": "https://www.flickr.com/photos/40295335@N00/4144789522"
+    },
+    "photo" : {
+      "author": "Olichel",
+      "url": "https://freerangestock.com/photographer/Olichel/4574"
     }
     
   }
 
+
+  Photo by <a target="_blank" href=>Olichel</a> from <a target="_blank" href="https://freerangestock.com">Freerange Stock</a>
+  Massachusetts Office Of Travel & Tourism, https://www.flickr.com/photos/masstravel/29796992180
+
+  
